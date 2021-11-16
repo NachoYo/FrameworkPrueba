d250d1fc9e8f5238efb6b0480daaeb56922e11d5
Luis Ignacio Viñas Petriz <luis.ignacio.vinas.petriz@everis.com>
2021-11-16 16:22:57 -0600

    afdsf

---
+++ Example/build/Pods.build/Debug-iphonesimulator/FrameworkPrueba.build/DerivedSources/FrameworkPrueba_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char FrameworkPruebaVersionString[];
+ extern const double FrameworkPruebaVersionNumber;
+
+ const unsigned char FrameworkPruebaVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:FrameworkPrueba  PROJECT:Pods-1" "\n";
+ const double FrameworkPruebaVersionNumber __attribute__ ((used)) = (double)1.;
